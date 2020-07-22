2593f8e2746dc649355b41c0a8040a741cd0e550
Luthaf <luthaf@luthaf.fr>
2020-07-22 16:55:27 +0200

    Add tests for dummy calculator

--- src/calculator/dummy_calculator.rs
+++ src/calculator/dummy_calculator.rs
@@ -3,13 +3,23 @@ use super::Calculator;
 use crate::descriptor::{Descriptor, IndexesBuilder, AtomIdx};
 use crate::system::System;
 
+/// A stupid calculator implementation used to test the API, and API binding to
+/// C/Python/etc. 
+///
+/// The calculator has two features: one containing the atom index +
+/// `self.delta`, and the other one containg `x + y + z`.
+#[doc(hidden)]
 #[derive(Debug, Clone)]
 #[derive(serde::Deserialize)]
 pub struct DummyCalculator {
-    cutoff: f64,
-    delta: usize,
-    name: String,
-    gradients: bool,
+    /// Spherical cutoff to use for atomic environments
+    pub cutoff: f64,
+    /// Delta added to the atom index in the first feature
+    pub delta: isize,
+    /// Unused name parameter, to test passing string values
+    pub name: String,
+    /// Should we also compute gradients of the feature?
+    pub gradients: bool,
 }
 
 impl Calculator for DummyCalculator {
@@ -33,10 +43,18 @@ impl Calculator for DummyCalculator {
             descriptor.prepare(environments, features, systems, 0.0);
         }
 
+        assert_eq!(descriptor.environments.names(), ["structure", "atom"]);
         for (i, indexes) in descriptor.environments.iter().enumerate() {
+            let mut current_structure = 0;
+            let mut positions = systems[current_structure].positions();
             if let &[structure, atom] = indexes {
-                descriptor.values[[i, 0]] = (atom + self.delta) as f64;
-                let position = systems[structure].positions()[atom];
+                if structure != current_structure {
+                    current_structure = structure;
+                    positions = systems[current_structure].positions();
+                }
+
+                descriptor.values[[i, 0]] = atom as f64 + self.delta as f64;
+                let position = positions[atom];
                 descriptor.values[[i, 1]] = position[0] + position[1] + position[2];
             } else {
                 unreachable!();
@@ -44,7 +62,83 @@ impl Calculator for DummyCalculator {
         }
 
         if self.gradients {
-            unimplemented!();
+            let gradients = descriptor.gradients.as_mut().expect("missing gradient values");
+            let gradients_indexes = descriptor.gradients_indexes.as_ref().expect("missing gradient index");
+
+            assert_eq!(gradients_indexes.names(), ["structure", "atom", "neighbor", "spatial"]);
+
+            for i in 0..gradients_indexes.count() {
+                gradients[[i, 0]] = 0.0;
+                gradients[[i, 1]] = 1.0;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::system::test_systems;
+    use crate::Descriptor;
+
+    use ndarray::{s, aview1};
+
+    use super::*;
+
+    #[test]
+    fn name() {
+        let calculator = DummyCalculator {
+            cutoff: 1.4,
+            delta: 9,
+            name: "a long name".into(),
+            gradients: false,
+        };
+
+        assert_eq!(
+            calculator.name(), 
+            "dummy test calculator with cutoff: 1.4 - delta: 9 - name: a long name - gradients: false"
+        );
+    }
+
+    #[test]
+    fn values() {
+        let mut calculator = DummyCalculator {
+            cutoff: 3.5,
+            delta: 9,
+            name: "".into(),
+            gradients: false,
+        };
+
+        let mut systems = test_systems(vec!["water"]);
+        let mut descriptor = Descriptor::new();
+        calculator.compute(&mut systems.get(), &mut descriptor);
+
+        assert_eq!(descriptor.values.slice(s![0, ..]), aview1(&[9.0, 0.0]));
+        assert_eq!(descriptor.values.slice(s![1, ..]), aview1(&[10.0, 0.16649999999999998]));
+        assert_eq!(descriptor.values.slice(s![2, ..]), aview1(&[11.0, -1.3443999999999998]));
+    }
+
+    #[test]
+    fn gradients() {
+        let mut calculator = DummyCalculator {
+            cutoff: 3.5,
+            delta: 0,
+            name: "".into(),
+            gradients: true,
+        };
+
+        let mut systems = test_systems(vec!["water"]);
+        let mut descriptor = Descriptor::new();
+        calculator.compute(&mut systems.get(), &mut descriptor);
+
+        assert_eq!(descriptor.values.slice(s![0, ..]), aview1(&[0.0, 0.0]));
+        assert_eq!(descriptor.values.slice(s![1, ..]), aview1(&[1.0, 0.16649999999999998]));
+        assert_eq!(descriptor.values.slice(s![2, ..]), aview1(&[2.0, -1.3443999999999998]));
+
+        let gradients = descriptor.gradients.unwrap();
+        // 1 (structure) * 3 (centers) * 2 (neighbor per center) * 3 (spatial)
+        assert_eq!(descriptor.gradients_indexes.unwrap().count(), 1 * 3 * 2 * 3);
+        for i in 0..gradients.shape()[0] {
+            assert_eq!(gradients.slice(s![i, ..]), aview1(&[0.0, 1.0]));
         }
     }
 }
