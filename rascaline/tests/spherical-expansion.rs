6b4d47471d77cd438bbbbeef608a5ec3d61e8fc9
Luthaf <luthaf@luthaf.fr>
2021-02-20 00:17:45 +0100

    Document how to add regression tests
    And improve saved files by making the file as platform independant as possible

--- rascaline/tests/spherical-expansion.rs
+++ rascaline/tests/spherical-expansion.rs
@@ -5,7 +5,7 @@ use rascaline::{System, Calculator, Descriptor};
 
 #[test]
 fn spherical_expansion() {
-    let (mut systems, parameters) = data::load_input("spherical-expansion-input.json");
+    let (mut systems, parameters) = data::load_input("spherical-expansion-values-input.json");
     let mut systems = systems.iter_mut().map(|s| s as &mut dyn System).collect::<Vec<_>>();
 
     let mut descriptor = Descriptor::new();
@@ -14,12 +14,6 @@ fn spherical_expansion() {
         .expect("failed to run calculation");
 
     let expected = data::load_expected_values("spherical-expansion-values.npy.gz");
-
     assert_eq!(descriptor.values.shape(), expected.shape());
-
-    for i in 0..expected.nrows() {
-        for j in 0..expected.ncols() {
-            assert_relative_eq!(descriptor.values[[i, j]], expected[[i, j]], max_relative=1e-12);
-        }
-    }
+    assert_relative_eq!(descriptor.values, expected, max_relative=1e-9);
 }
