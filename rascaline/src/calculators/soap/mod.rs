e4324fa63a308ef01720f5c9c804947b231c80a8
Guillaume Fraux <guillaume.fraux@epfl.ch>
2021-03-11 18:52:03 +0100

    Refactor radial integral to a separate module

--- rascaline/src/calculators/soap/mod.rs
+++ rascaline/src/calculators/soap/mod.rs
@@ -1,9 +1,7 @@
-mod hypergeometric;
-pub use self::hypergeometric::{HyperGeometricSphericalExpansion, HyperGeometricParameters};
-
 mod radial_integral;
 pub use self::radial_integral::RadialIntegral;
 pub use self::radial_integral::{GtoRadialIntegral, GtoParameters};
+pub use self::radial_integral::{HyperGeometricSphericalExpansion, HyperGeometricParameters};
 
 mod spherical_harmonics;
 pub use self::spherical_harmonics::{SphericalHarmonics, SphericalHarmonicsArray};
---
+++ rascaline/src/calculators/soap/radial_integral/mod.rs
@@ -0,0 +1,14 @@
+use ndarray::ArrayViewMut2;
+
+pub trait RadialIntegral: std::panic::RefUnwindSafe {
+    /// Compute the radial integral for a single atomic `distance` and store the
+    /// resulting data in the `max_radial x max_angular` array `values`. If
+    /// `gradients` is `Some`, also compute and store gradients.
+    fn compute(&self, rij: f64, values: ArrayViewMut2<f64>, gradients: Option<ArrayViewMut2<f64>>);
+}
+
+mod hypergeometric;
+pub use self::hypergeometric::{HyperGeometricSphericalExpansion, HyperGeometricParameters};
+
+mod gto;
+pub use self::gto::{GtoRadialIntegral, GtoParameters};
