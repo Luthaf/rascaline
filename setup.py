6227cd96b42e6024264370e2a52d7904774dbe5e
Guillaume Fraux <guillaume.fraux@epfl.ch>
2021-06-15 12:10:31 +0200

    Use the cmake build system to build the lib in python
    This removed duplication of functionalities such as disabling chemfiles
    or using `@rpath/librascaline.dylib` as the link id on macOS
    
    This also install headers & cmake configuration inside the python module,
    allowing to use it to link another project (such as the pytorch integration)
    with rascaline

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 # -*- coding=utf-8 -*-
 import os
 import sys
-import subprocess
 
-from setuptools import setup
-from setuptools.dist import Distribution
+from skbuild import setup
 from wheel.bdist_wheel import bdist_wheel
-from distutils.command.build_ext import build_ext
 
 ROOT = os.path.realpath(os.path.dirname(__file__))
 
 if sys.version_info < (3, 6):
     sys.exit("Sorry, Python < 3.6 is not supported")
 
+# do not include chemfiles inside rascaline, instead users should use
+# chemfiles python bindings directly
+cmake_args = ["-DRASCAL_DISABLE_CHEMFILES=ON"]
 
 RASCALINE_BUILD_TYPE = os.environ.get("RASCALINE_BUILD_TYPE", "release")
 if RASCALINE_BUILD_TYPE not in ["debug", "release"]:
@@ -21,73 +21,27 @@ if RASCALINE_BUILD_TYPE not in ["debug", "release"]:
         "expected 'debug' or 'release'"
     )
 
-
-class BinaryDistribution(Distribution):
-    """
-    This is necessary because otherwise the wheel does not know that
-    it contains a compiled module
-    """
-
-    def has_ext_modules(self):
-        return True
+cmake_args.append(f"-DCMAKE_BUILD_TYPE={RASCALINE_BUILD_TYPE}")
 
 
 class universal_wheel(bdist_wheel):
-    # Workaround until https://github.com/pypa/wheel/issues/185 is resolved
+    # When building the wheel, the `wheel` package assumes that if we have a
+    # binary extension then we are linking to `libpython.so`; and thus the wheel
+    # is only usable with a single python version. This is not the case for
+    # here, and the wheel will be compatible with any Python >=3.6. This is
+    # tracked in https://github.com/pypa/wheel/issues/185, but until then we
+    # manually override the wheel tag.
     def get_tag(self):
         tag = bdist_wheel.get_tag(self)
+        # tag[2:] contains the os/arch tags, we want to keep them
         return ("py3", "none") + tag[2:]
 
 
-class cargo_ext(build_ext):
-    """
-    Build rust code using cargo
-    """
-
-    def run(self):
-        if sys.platform.startswith("darwin"):
-            dylib = "librascaline.dylib"
-        elif sys.platform.startswith("linux"):
-            dylib = "librascaline.so"
-        elif sys.platform.startswith("win"):
-            dylib = "librascaline.dll"
-        else:
-            raise ImportError("Unknown platform. Please edit this file")
-
-        cargo_build = [
-            "cargo",
-            "build",
-            # do not include chemfiles when building the Python package
-            "--no-default-features",
-        ]
-        if RASCALINE_BUILD_TYPE == "release":
-            cargo_build.append("--release")
-
-        process = subprocess.Popen(
-            cargo_build, cwd=os.path.join(ROOT, "rascaline-c-api")
-        )
-        status = process.wait()
-        if status != 0:
-            sys.exit(status)
-
-        dst = os.path.join(self.build_lib, "rascaline", dylib)
-        try:
-            os.makedirs(os.path.dirname(dst))
-        except OSError:
-            pass
-
-        src = os.path.join(ROOT, "target", RASCALINE_BUILD_TYPE, dylib)
-        if os.path.isfile(src):
-            self.copy_file(src, dst)
-        else:
-            raise Exception("Failed to build rust code")
-
-
 setup(
-    distclass=BinaryDistribution,
     ext_modules=[],
     cmdclass={
-        "build_ext": cargo_ext,
         "bdist_wheel": universal_wheel,
     },
+    cmake_source_dir="rascaline-c-api",
+    cmake_args=cmake_args,
 )
