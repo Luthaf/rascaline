bdc9eec345bbfeb0f2f7b4f5530495ce5fe927b3
Guillaume Fraux <guillaume.fraux@epfl.ch>
2021-09-23 12:04:11 +0200

    Improve API documentation

--- rascaline/src/calculators/soap/radial_integral/mod.rs
+++ rascaline/src/calculators/soap/radial_integral/mod.rs
@@ -1,9 +1,18 @@
 use ndarray::ArrayViewMut2;
 
+/// A `RadialIntegral` computes the radial integral on a given radial basis.
+///
+/// See equations 5 to 8 of [this paper](https://doi.org/10.1063/5.0044689) for
+/// mor information on the radial integral.
+///
+/// `std::panic::RefUnwindSafe` is a required super-trait to enable passing
+/// radial integrals across the C API. `Send` is a required super-trait to
+/// enable passing radial integrals between threads.
 pub trait RadialIntegral: std::panic::RefUnwindSafe + Send {
-    /// Compute the radial integral for a single atomic `distance` and store the
-    /// resulting data in the `max_radial x max_angular` array `values`. If
-    /// `gradients` is `Some`, also compute and store gradients.
+    /// Compute the radial integral for a single `distance` between two atoms
+    /// and store the resulting data in the `max_radial x (max_angular + 1)`
+    /// array `values`. If `gradients` is `Some`, also compute and store
+    /// gradients there.
     fn compute(&self, rij: f64, values: ArrayViewMut2<f64>, gradients: Option<ArrayViewMut2<f64>>);
 }
 
