584101e6205db5f752f9945f96bc14e47eedc258
Guillaume Fraux <guillaume.fraux@epfl.ch>
2022-06-17 15:11:05 +0200

    Add a test installing and using the cmake project

--- rascaline-c-api/cmake/rascaline-config.in.cmake
+++ rascaline-c-api/cmake/rascaline-config.in.cmake
@@ -4,8 +4,24 @@ if(rascaline_FOUND)
     return()
 endif()
 
-add_library(rascaline SHARED IMPORTED GLOBAL)
+enable_language(CXX)
+
+if (@BUILD_SHARED_LIBS@)
+    add_library(rascaline SHARED IMPORTED GLOBAL)
+else()
+    add_library(rascaline STATIC IMPORTED GLOBAL)
+endif()
+
 set_target_properties(rascaline PROPERTIES
     IMPORTED_LOCATION ${PACKAGE_PREFIX_DIR}/@LIB_INSTALL_DIR@/@RASCALINE_LIB_NAME@
     INTERFACE_INCLUDE_DIRECTORIES ${PACKAGE_PREFIX_DIR}/@INCLUDE_INSTALL_DIR@/
+    # we need to link with a C++ compiler to get the C++ stdlib for chemfiles
+    IMPORTED_LINK_INTERFACE_LANGUAGES CXX
 )
+
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux" AND NOT @BUILD_SHARED_LIBS@)
+    set(THREADS_PREFER_PTHREAD_FLAG ON)
+    find_package(Threads REQUIRED)
+    # the rust standard lib uses pthread and libdl on linux
+    target_link_libraries(rascaline INTERFACE Threads::Threads dl)
+endif()
--- rascaline-c-api/tests/python-api.rs
+++ rascaline-c-api/tests/python-api.rs
@@ -5,12 +5,12 @@ mod utils;
 
 #[test]
 fn check_python() {
-    utils::check_command_exists("tox");
+    let tox = which::which("tox").expect("could not find tox");
 
     let mut root = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
     root.pop();
 
-    let mut tox = Command::new("tox");
+    let mut tox = Command::new(tox);
     tox.arg("--");
     if cfg!(debug_assertions) {
         // assume that debug assertion means that we are building the code in
