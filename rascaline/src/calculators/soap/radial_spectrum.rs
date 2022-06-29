05f9d14864965e67f530e213c125a926145ba311
Guillaume Fraux <guillaume.fraux@epfl.ch>
2022-06-29 15:51:17 +0200

    Reformat code

--- rascaline/src/calculators/soap/radial_spectrum.rs
+++ rascaline/src/calculators/soap/radial_spectrum.rs
@@ -1,18 +1,18 @@
 use std::sync::Arc;
 
-use equistore::{TensorMap, TensorBlock, EmptyArray};
-use equistore::{LabelsBuilder, Labels, LabelValue};
+use equistore::{EmptyArray, TensorBlock, TensorMap};
+use equistore::{LabelValue, Labels, LabelsBuilder};
 
 use crate::calculators::CalculatorBase;
 use crate::{CalculationOptions, Calculator, LabelsSelection};
 use crate::{Error, System};
 
 use super::SphericalExpansionParameters;
-use super::{SphericalExpansion, RadialBasis, CutoffFunction, RadialScaling};
+use super::{CutoffFunction, RadialBasis, RadialScaling, SphericalExpansion};
 
-use crate::labels::{SpeciesFilter, SamplesBuilder};
 use crate::labels::AtomCenteredSamples;
-use crate::labels::{KeysBuilder, CenterSingleNeighborsSpeciesKeys};
+use crate::labels::{CenterSingleNeighborsSpeciesKeys, KeysBuilder};
+use crate::labels::{SamplesBuilder, SpeciesFilter};
 
 
 /// Parameters for Radial spectrum calculator.
@@ -88,25 +88,37 @@ impl SoapRadialSpectrum {
 
     /// Construct a `TensorMap` containing the set of samples/properties we want the
     /// spherical expansion to compute.
-    fn selected_spx_labels(&self, descriptor: &TensorMap) -> TensorMap {
-        assert_eq!(descriptor.keys().names(), ["species_center", "species_neighbor"]);
+    fn selected_spx_labels(descriptor: &TensorMap) -> TensorMap {
+        assert_eq!(
+            descriptor.keys().names(),
+            ["species_center", "species_neighbor"]
+        );
 
-        let mut keys_builder = LabelsBuilder::new(vec!["spherical_harmonics_l", "species_center", "species_neighbor"]);
+        let mut keys_builder = LabelsBuilder::new(vec![
+            "spherical_harmonics_l",
+            "species_center",
+            "species_neighbor",
+        ]);
         let mut blocks = Vec::new();
         for (&[center, neighbor], block) in descriptor.keys().iter_fixed_size().zip(descriptor.blocks()) {
-            keys_builder.add(&[LabelValue::new(0),center, neighbor]);
+            // spherical_harmonics_l is always 0
+            keys_builder.add(&[LabelValue::new(0), center, neighbor]);
 
-            blocks.push(TensorBlock::new(
-                EmptyArray::new(vec![block.values().samples.count(), block.values().properties.count()]),
-                Arc::clone(&block.values().samples),
-                Vec::new(),
-                Arc::clone(&block.values().properties),
-            ).expect("invalid TensorBlock"));
+            blocks.push(
+                TensorBlock::new(
+                    EmptyArray::new(vec![
+                        block.values().samples.count(),
+                        block.values().properties.count(),
+                    ]),
+                    Arc::clone(&block.values().samples),
+                    Vec::new(),
+                    Arc::clone(&block.values().properties),
+                ).expect("invalid TensorBlock")
+            );
         }
 
-        return TensorMap::new(keys_builder.finish(), blocks).expect("invalid TensorMap")
+        return TensorMap::new(keys_builder.finish(), blocks).expect("invalid TensorMap");
     }
-
 }
 
 impl CalculatorBase for SoapRadialSpectrum {
@@ -130,11 +142,14 @@ impl CalculatorBase for SoapRadialSpectrum {
         AtomCenteredSamples::samples_names()
     }
 
-    fn samples(&self, keys: &equistore::Labels, systems: &mut [Box<dyn System>]) -> Result<Vec<Arc<Labels>>, Error> {
+    fn samples(
+        &self,
+        keys: &equistore::Labels,
+        systems: &mut [Box<dyn System>],
+    ) -> Result<Vec<Arc<Labels>>, Error> {
         assert_eq!(keys.names(), ["species_center", "species_neighbor"]);
         let mut result = Vec::new();
         for [species_center, species_neighbor] in keys.iter_fixed_size() {
-
             let builder = AtomCenteredSamples {
                 cutoff: self.parameters.cutoff,
                 species_center: SpeciesFilter::Single(species_center.i32()),
@@ -148,7 +163,12 @@ impl CalculatorBase for SoapRadialSpectrum {
         return Ok(result);
     }
 
-    fn gradient_samples(&self, keys: &Labels, samples: &[Arc<Labels>], systems: &mut [Box<dyn System>]) -> Result<Option<Vec<Arc<Labels>>>, Error> {
+    fn gradient_samples(
+        &self,
+        keys: &Labels,
+        samples: &[Arc<Labels>],
+        systems: &mut [Box<dyn System>],
+    ) -> Result<Option<Vec<Arc<Labels>>>, Error> {
         assert_eq!(keys.names(), ["species_center", "species_neighbor"]);
         assert_eq!(keys.count(), samples.len());
 
@@ -181,11 +201,10 @@ impl CalculatorBase for SoapRadialSpectrum {
 
     fn properties(&self, keys: &equistore::Labels) -> Vec<Arc<Labels>> {
         let mut properties = LabelsBuilder::new(self.properties_names());
-        
         for n in 0..self.parameters.max_radial {
             properties.add(&[n]);
         }
-        
+
         let properties = Arc::new(properties.finish());
 
         return vec![properties; keys.count()];
@@ -193,8 +212,9 @@ impl CalculatorBase for SoapRadialSpectrum {
 
     #[time_graph::instrument(name = "SoapRadialSpectrum::compute")]
     fn compute(&mut self, systems: &mut [Box<dyn System>], descriptor: &mut TensorMap) -> Result<(), Error> {
-        assert_eq!(descriptor.keys().names(), [ "species_center", "species_neighbor"]);
-        let selected = self.selected_spx_labels(descriptor);
+        assert_eq!(descriptor.keys().names(), ["species_center", "species_neighbor"]);
+
+        let selected = SoapRadialSpectrum::selected_spx_labels(descriptor);
         let options = CalculationOptions {
             selected_samples: LabelsSelection::Predefined(&selected),
             selected_properties: LabelsSelection::Predefined(&selected),
@@ -206,22 +226,34 @@ impl CalculatorBase for SoapRadialSpectrum {
             options,
         ).expect("failed to compute spherical expansion");
 
-        for ( (_,mut block_d),(_, block_spx)) in descriptor.iter_mut().zip(spherical_expansion.iter()) {
-            let arr = block_d.values_mut().data.as_array_mut();
-            let spx = block_spx.values().data.as_array();
-            let dim = spx.shape();
-            let tmp = spx.view().into_shape((dim[0],dim[2])).expect("wrong shape");
-            arr.assign(&tmp);
+        for ((_, mut block), (_, block_spx)) in
+            descriptor.iter_mut().zip(spherical_expansion.iter())
+        {
+            let array = block.values_mut().data.as_array_mut();
+            let array_spx = block_spx.values().data.as_array();
+            let shape = array_spx.shape();
+            // shape[1] is the m component
+            debug_assert_eq!(shape[1], 1);
+            let array_spx_reshaped = array_spx.view().into_shape(
+                (shape[0], shape[2])
+            ).expect("wrong shape");
+            array.assign(&array_spx_reshaped);
 
             if self.parameters.gradients {
-                let gradients = block_d.gradient_mut("positions").expect("missing radial spectrum gradients");
-                let spx_grad = block_spx.gradient("positions").expect("missing spherical expansion gradients");
-                debug_assert_eq!(gradients.samples, spx_grad.samples);
-                let arr_grad_data = gradients.data.as_array_mut();
-                let spx_grad_data = spx_grad.data.as_array();
-                let dim_grad = spx_grad_data.shape();
-                let tmp_grad = spx_grad_data.view().into_shape((dim_grad[0],dim_grad[1],dim_grad[3])).expect("wrong shape");
-                arr_grad_data.assign(&tmp_grad);
+                let gradient = block.gradient_mut("positions").expect("missing radial spectrum gradients");
+                let gradient_spx = block_spx.gradient("positions").expect("missing spherical expansion gradients");
+                debug_assert_eq!(gradient.samples, gradient_spx.samples);
+
+                let array = gradient.data.as_array_mut();
+                let array_spx = gradient_spx.data.as_array();
+                let shape = array_spx.shape();
+                // shape[2] is the m component
+                debug_assert_eq!(shape[2], 1);
+
+                let array_spx_reshaped = array_spx.view().into_shape(
+                    (shape[0], shape[1], shape[3])
+                ).expect("wrong shape");
+                array.assign(&array_spx_reshaped);
             }
         }
 
@@ -229,12 +261,11 @@ impl CalculatorBase for SoapRadialSpectrum {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use equistore::LabelValue;
 
-    use crate::systems::test_utils::{test_systems, test_system};
+    use crate::systems::test_utils::{test_system, test_systems};
     use crate::Calculator;
 
     use super::*;
@@ -255,34 +286,25 @@ mod tests {
 
     #[test]
     fn values() {
-        let mut calculator = Calculator::from(Box::new(SoapRadialSpectrum::new(
-            parameters(false)
-        ).unwrap()) as Box<dyn CalculatorBase>);
+        let mut calculator = Calculator::from(Box::new(
+            SoapRadialSpectrum::new(parameters(false)).unwrap()
+        ) as Box<dyn CalculatorBase>);
 
         let mut systems = test_systems(&["water"]);
         let descriptor = calculator.compute(&mut systems, Default::default()).unwrap();
 
         assert_eq!(descriptor.keys().count(), 4);
-        assert!(descriptor.keys().contains(
-            &[LabelValue::new(1), LabelValue::new(1)]
-        ));
-        assert!(descriptor.keys().contains(
-            &[LabelValue::new(1), LabelValue::new(-42), ]
-        ));
-
-        assert!(descriptor.keys().contains(
-            &[LabelValue::new(-42), LabelValue::new(1)]
-        ));
-        assert!(descriptor.keys().contains(
-            &[LabelValue::new(-42), LabelValue::new(-42)]
-        ));
+        assert!(descriptor.keys().contains(&[LabelValue::new(1), LabelValue::new(1)]));
+        assert!(descriptor.keys().contains(&[LabelValue::new(1), LabelValue::new(-42)]));
+        assert!(descriptor.keys().contains(&[LabelValue::new(-42), LabelValue::new(1)]));
+        assert!(descriptor.keys().contains(&[LabelValue::new(-42), LabelValue::new(-42)]));
     }
 
     #[test]
     fn finite_differences() {
-        let calculator = Calculator::from(Box::new(SoapRadialSpectrum::new(
-            parameters(true)
-        ).unwrap()) as Box<dyn CalculatorBase>);
+        let calculator = Calculator::from(Box::new(
+            SoapRadialSpectrum::new(parameters(true)).unwrap()
+        ) as Box<dyn CalculatorBase>);
 
         let system = test_system("water");
         let options = crate::calculators::tests_utils::FinalDifferenceOptions {
@@ -295,17 +317,17 @@ mod tests {
 
     #[test]
     fn compute_partial() {
-        let calculator = Calculator::from(Box::new(SoapRadialSpectrum::new(
-            parameters(false)
-        ).unwrap()) as Box<dyn CalculatorBase>);
+        let calculator = Calculator::from(Box::new(
+            SoapRadialSpectrum::new(parameters(false)).unwrap()
+        ) as Box<dyn CalculatorBase>);
 
         let mut systems = test_systems(&["water", "methane"]);
 
-        let mut properties = LabelsBuilder::new(vec![ "n"]);
-        properties.add(&[ LabelValue::new(0)]);
-        properties.add(&[ LabelValue::new(3)]);
-        properties.add(&[ LabelValue::new(4)]);
-        properties.add(&[ LabelValue::new(1)]);
+        let mut properties = LabelsBuilder::new(vec!["n"]);
+        properties.add(&[LabelValue::new(0)]);
+        properties.add(&[LabelValue::new(3)]);
+        properties.add(&[LabelValue::new(4)]);
+        properties.add(&[LabelValue::new(1)]);
 
         let mut samples = LabelsBuilder::new(vec!["structure", "center"]);
         samples.add(&[LabelValue::new(0), LabelValue::new(1)]);
@@ -313,32 +335,10 @@ mod tests {
         samples.add(&[LabelValue::new(1), LabelValue::new(0)]);
 
         crate::calculators::tests_utils::compute_partial(
-            calculator, &mut systems, &samples.finish(), &properties.finish()
+            calculator,
+            &mut systems,
+            &samples.finish(),
+            &properties.finish(),
         );
-    }
-
-    #[test]
-    fn center_atom_weight() {
-        let system = &mut test_systems(&["CH"]);
-
-        let mut parameters = parameters(false);
-        parameters.cutoff = 0.5;
-        parameters.center_atom_weight = 1.0;
-
-        let mut calculator = Calculator::from(Box::new(
-            SoapRadialSpectrum::new(parameters.clone()).unwrap(),
-        ) as Box<dyn CalculatorBase>);
-        let descriptor = calculator.compute(system, Default::default()).unwrap();
-
-        parameters.center_atom_weight = 0.5;
-        let mut calculator = Calculator::from(Box::new(
-            SoapRadialSpectrum::new(parameters).unwrap(),
-        ) as Box<dyn CalculatorBase>);
-
-        let descriptor_scaled = calculator.compute(system, Default::default()).unwrap();
-
-        for (block, block_scaled) in descriptor.blocks().iter().zip(descriptor_scaled.blocks()) {
-            assert_eq!(block.values().data.as_array(), 4.0 * block_scaled.values().data.as_array());
-        }
     }
 }
