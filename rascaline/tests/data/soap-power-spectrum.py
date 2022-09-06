ceb9ce941c5470f3026c8983b66f2ecb85db86ff
Guillaume Fraux <guillaume.fraux@epfl.ch>
2022-09-06 18:43:05 +0200

    Use splines by default in the SOAP radial integral

--- rascaline/tests/data/soap-power-spectrum.py
+++ rascaline/tests/data/soap-power-spectrum.py
@@ -29,7 +29,9 @@ hyperparameters = {
     "atomic_gaussian_width": 0.3,
     "center_atom_weight": 1.0,
     "radial_basis": {
-        "Gto": {},
+        "Gto": {
+            "splined_radial_integral": False,
+        },
     },
     "cutoff_function": {
         "ShiftedCosine": {
