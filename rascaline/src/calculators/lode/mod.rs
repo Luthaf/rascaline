e365e358ae62d8d7653d313dedd4620b69cb7976
Guillaume Fraux <guillaume.fraux@epfl.ch>
2022-09-07 16:51:42 +0200

    Implement projection of k-vectors to nlm

--- rascaline/src/calculators/lode/mod.rs
+++ rascaline/src/calculators/lode/mod.rs
@@ -1,2 +1,8 @@
+mod gto_radial_integral;
+pub use self::gto_radial_integral::{LodeGtoRadialIntegral, GtoParameters};
+
+mod radial_basis;
+pub use self::radial_basis::LodeRadialBasis;
+
 mod spherical_expansion;
 pub use self::spherical_expansion::{LodeSphericalExpansion, LodeSphericalExpansionParameters};
