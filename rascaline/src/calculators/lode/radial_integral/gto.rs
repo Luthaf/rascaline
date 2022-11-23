b37e54c5a39b7338c0fd6431297cbddc45d99874
Guillaume Fraux <guillaume.fraux@epfl.ch>
2022-11-23 16:28:29 +0100

    Move the central atom comtribution to radial basis trait

--- rascaline/src/calculators/lode/radial_integral/gto.rs
+++ rascaline/src/calculators/lode/radial_integral/gto.rs
@@ -1,14 +1,57 @@
 use std::f64;
 
-use ndarray::{Array2, ArrayViewMut2};
+use ndarray::{Array1, Array2, ArrayViewMut2};
 
-use crate::math::{gamma, DoubleRegularized1F1};
+use crate::math::{hyp2f1, gamma, DoubleRegularized1F1};
 use crate::Error;
 
 use super::LodeRadialIntegral;
 use crate::calculators::radial_basis::GtoRadialBasis;
 
-pub use crate::calculators::soap::SoapRadialIntegralGtoParameters as LodeRadialIntegralGtoParameters;
+/// Parameters controlling the LODE radial integral with GTO radial basis
+#[derive(Debug, Clone, Copy)]
+pub struct LodeRadialIntegralGtoParameters {
+    /// Number of radial components
+    pub max_radial: usize,
+    /// Number of angular components
+    pub max_angular: usize,
+    /// atomic density gaussian width
+    pub atomic_gaussian_width: f64,
+    /// potential exponent
+    pub potential_exponent: usize,
+    /// cutoff radius
+    pub cutoff: f64,
+}
+
+impl LodeRadialIntegralGtoParameters {
+    pub(crate) fn validate(&self) -> Result<(), Error> {
+        if self.max_radial == 0 {
+            return Err(Error::InvalidParameter(
+                "max_radial must be at least 1 for GTO radial integral".into()
+            ));
+        }
+
+        if self.cutoff < 0.0 || !self.cutoff.is_finite() {
+            return Err(Error::InvalidParameter(
+                "cutoff must be a positive number for GTO radial integral".into()
+            ));
+        }
+
+        if self.atomic_gaussian_width < 0.0 || !self.atomic_gaussian_width.is_finite() {
+            return Err(Error::InvalidParameter(
+                "atomic_gaussian_width must be a positive number for GTO radial integral".into()
+            ));
+        }
+
+        if self.potential_exponent > 6 {
+            return Err(Error::InvalidParameter(
+                "potential_exponent larger than 6 is not implemented for GTO radial integral".into()
+            ));
+        }
+
+        Ok(())
+    }
+}
 
 /// Implementation of the LODE radial integral for GTO radial basis and Gaussian
 /// atomic density.
@@ -121,6 +164,56 @@ impl LodeRadialIntegral for LodeRadialIntegralGto {
             gradients.assign(&gradients.dot(&self.gto_orthonormalization));
         }
     }
+
+    fn compute_center_contribution(&self) -> Array1<f64> {
+        let max_radial = self.parameters.max_radial;
+        let atomic_gaussian_width = self.parameters.atomic_gaussian_width;
+        let potential_exponent = self.parameters.potential_exponent as f64;
+
+        let mut contrib = Array1::from_elem(max_radial, 0.0);
+
+        let basis = GtoRadialBasis {
+            max_radial,
+            cutoff: self.parameters.cutoff,
+        };
+        let gto_gaussian_widths = basis.gaussian_widths();
+        let n_eff: Vec<f64> = (0..max_radial)
+            .into_iter()
+            .map(|n| 0.5 * (3. + n as f64))
+            .collect();
+
+        if potential_exponent == 0. {
+            let factor = std::f64::consts::PI.powf(-0.25)
+                / (atomic_gaussian_width * atomic_gaussian_width).powf(0.75);
+
+            for n in 0..max_radial {
+                let alpha = 0.5
+                    * (1. / (atomic_gaussian_width * atomic_gaussian_width)
+                        + 1. / (gto_gaussian_widths[n] * gto_gaussian_widths[n]));
+                contrib[n] = factor * gamma(n_eff[n]) / alpha.powf(n_eff[n]);
+            }
+        } else {
+            let factor = 2. * f64::sqrt(4. * std::f64::consts::PI)
+                / gamma(potential_exponent / 2.)
+                / potential_exponent;
+
+            for n in 0..max_radial {
+                let s = atomic_gaussian_width / gto_gaussian_widths[n];
+                let hyparg = 1. / (1. + s * s);
+
+                contrib[n] = factor
+                    * 2_f64.powf((1. + n as f64 - potential_exponent) / 2.)
+                    * atomic_gaussian_width.powi(3 + n as i32 - potential_exponent as i32)
+                    * gamma(n_eff[n])
+                    * hyp2f1(1., n_eff[n], (potential_exponent + 2.) / 2., hyparg)
+                    * hyparg.powf(n_eff[n]);
+            }
+        }
+
+        let gto_orthonormalization = basis.orthonormalization_matrix();
+
+        return gto_orthonormalization.dot(&(contrib));
+    }
 }
 
 #[cfg(test)]
@@ -139,6 +232,7 @@ mod tests {
             max_angular: max_angular,
             cutoff: 5.0,
             atomic_gaussian_width: 0.5,
+            potential_exponent: 1,
         }).unwrap();
 
         let shape = (max_angular + 1, max_radial);
@@ -162,6 +256,7 @@ mod tests {
             max_angular: max_angular,
             cutoff: 5.0,
             atomic_gaussian_width: 0.5,
+            potential_exponent: 1,
         }).unwrap();
 
         let k = 3.4;
@@ -179,5 +274,30 @@ mod tests {
         assert_relative_eq!(
             finite_differences, gradients, max_relative=1e-4
         );
+    }
+
+    #[test]
+    fn central_atom_contribution() {
+        let potential_exponents = [0, 1, 2, 6];
+
+        // Reference values taken from pyLODE
+        let reference_vals = [
+            [7.09990773e-01, 6.13767550e-01, 3.34161655e-01, 8.35301652e-02, 1.78439072e-02, -3.44944648e-05],
+            [1.69193719, 2.02389574, 2.85086136, 3.84013091, 1.62869125, 7.03338899],
+            [1.00532822, 1.10024472, 1.34843326, 1.19816598, 0.69150744, 1.2765415],
+            [0.03811939, 0.03741200, 0.03115835, 0.01364843, 0.00534184, 0.00205973]];
+
+        for (i, &p) in potential_exponents.iter().enumerate(){
+            let gto = LodeRadialIntegralGto::new(LodeRadialIntegralGtoParameters {
+                cutoff: 5.0,
+                max_radial: 6,
+                max_angular: 2,
+                atomic_gaussian_width: 1.0,
+                potential_exponent: p,
+            }).unwrap();
+
+            let center_contrib = gto.compute_center_contribution();
+            assert_relative_eq!(center_contrib, ndarray::arr1(&reference_vals[i]), max_relative=3e-6);
+        };
     }
 }
