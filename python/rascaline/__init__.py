5df9d027e57f0b339f25c5498011262585b10382
Guillaume Fraux <guillaume.fraux@epfl.ch>
2023-06-20 12:02:36 +0200

    Stop using deprecated pkg_ressources

--- python/rascaline/__init__.py
+++ python/rascaline/__init__.py
@@ -1,11 +1,8 @@
-import os
-
-from pkg_resources import DistributionNotFound, get_distribution
-
 from .log import set_logging_callback  # noqa
 from .profiling import Profiler  # noqa
 from .status import RascalError  # noqa
 from .systems import IntoSystem, SystemBase  # noqa
+from .version import __version__  # noqa
 
 
 from .calculators import CalculatorBase  # noqa  isort: skip
@@ -19,19 +16,3 @@ from .calculators import SoapRadialSpectrum  # noqa  isort: skip
 from .calculators import SoapPowerSpectrum  # noqa  isort: skip
 
 from .splines import generate_splines  # noqa  isort: skip
-
-
-# Get the __version__ attribute from setuptools metadata (which took it from
-# Cargo.toml) cf https://stackoverflow.com/a/17638236/4692076
-try:
-    dist = get_distribution("rascaline")
-    # Normalize case for Windows systems
-    dist_loc = os.path.normcase(dist.location)
-    here = os.path.normcase(__file__)
-    if not here.startswith(os.path.join(dist_loc, "rascaline")):
-        # not installed, but there is another version that *is*
-        raise DistributionNotFound
-except DistributionNotFound:
-    __version__ = "dev"
-else:
-    __version__ = dist.version
