1a8a1800475069a94a3b92fa208e875b69f78ffe
Guillaume Fraux <guillaume.fraux@epfl.ch>
2023-07-12 16:18:28 +0200

    Add basic package structure for rascaline-torch

---
+++ rascaline-torch/include/rascaline/torch.hpp
@@ -0,0 +1,8 @@
+#ifndef RASCALINE_TORCH_HPP
+#define RASCALINE_TORCH_HPP
+
+#include "torch/system.hpp"
+#include "torch/calculator.hpp"
+
+
+#endif
---
+++ rascaline-torch/src/calculator.cpp
@@ -0,0 +1,1 @@
+#include "rascaline/torch/calculator.hpp"
