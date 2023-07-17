3c0c4b19d196eb0c3807165272044cbfb20e5605
Guillaume Fraux <guillaume.fraux@epfl.ch>
2023-07-17 14:48:01 +0200

    Calculator and autograd integration for rascaline-torch

---
+++ rascaline-torch/src/autograd.cpp
@@ -0,0 +1,237 @@
+#include "rascaline/torch/autograd.hpp"
+
+using namespace rascaline_torch;
+
+#define stringify(str) #str
+#define always_assert(condition)                                               \
+    do {                                                                       \
+        if (!(condition)) {                                                    \
+            throw std::runtime_error(                                          \
+                std::string("assert failed: ") + stringify(condition)          \
+            );                                                                 \
+        }                                                                      \
+    } while (false)
+
+static bool all_systems_use_native(const std::vector<TorchSystem>& systems) {
+    auto result = systems[0]->use_native_system();
+    for (const auto& system: systems) {
+        if (system->use_native_system() != result) {
+            C10_THROW_ERROR(ValueError,
+                "either all or none of the systems should have pre-defined neighbor lists"
+            );
+        }
+    }
+    return result;
+}
+
+std::vector<torch::Tensor> RascalineAutograd::forward(
+    torch::autograd::AutogradContext *ctx,
+    torch::Tensor all_positions,
+    torch::Tensor all_cells,
+    CalculatorHolder& calculator,
+    std::vector<TorchSystem> systems,
+    equistore_torch::TorchTensorMap* tensor_map
+) {
+    auto calculation_options = rascaline::CalculationOptions();
+    if (all_positions.requires_grad()) {
+        calculation_options.gradients.push_back("positions");
+    }
+
+    if (all_cells.requires_grad()) {
+        calculation_options.gradients.push_back("cell");
+    }
+
+    calculation_options.use_native_system = all_systems_use_native(systems);
+    // TODO: selected_properties
+    // TODO: selected_samples
+    // TODO: keep_forward_gradients
+
+    auto structures_start = std::vector<int64_t>();
+    int64_t current_start = 0;
+    for (auto& system: systems) {
+        structures_start.push_back(current_start);
+        current_start += system->size();
+    }
+
+    // run the actual calculation
+    auto descriptor = calculator.compute_impl(systems, calculation_options);
+
+    // extract the data for autograd
+    auto values_by_block = std::vector<torch::Tensor>();
+    for (int64_t i=0; i<descriptor->keys()->count(); i++) {
+        // this add a reference to the torch::Tensor already in `descriptor`
+        // inside `values_by_block`
+        values_by_block.push_back(descriptor->block_by_id(i)->values());
+    }
+
+    // save the required data for backward pass
+    ctx->save_for_backward({all_positions, all_cells});
+    ctx->saved_data["descriptor"] = descriptor;
+    ctx->saved_data["structures_start"] = structures_start;
+
+    *tensor_map = std::move(descriptor);
+
+    return values_by_block;
+}
+
+torch::autograd::variable_list RascalineAutograd::backward(
+    torch::autograd::AutogradContext *ctx,
+    torch::autograd::variable_list grad_outputs
+) {
+    /* get the saved data from the forward pass */
+    auto saved_variables = ctx->get_saved_variables();
+    auto all_positions = saved_variables[0];
+    auto all_cells = saved_variables[1];
+
+    auto descriptor = ctx->saved_data["descriptor"].toCustomClass<equistore_torch::TensorMapHolder>();
+    auto structures_start = ctx->saved_data["structures_start"].toIntVector();
+
+    auto n_blocks = grad_outputs.size();
+    always_assert(descriptor->keys()->count() == n_blocks);
+
+    for (size_t i=0; i<n_blocks; i++) {
+        // TODO: do not make everything contiguous, instead check how much
+        // slower `torch::dot` is here.
+        grad_outputs[i] = grad_outputs[i].contiguous();
+    }
+
+    /* gradient w.r.t. positions */
+    auto positions_grad = torch::Tensor();
+    if (all_positions.requires_grad()) {
+        positions_grad = torch::zeros_like(all_positions);
+        assert(positions_grad.is_contiguous() && positions_grad.is_cpu());
+        auto positions_grad_ptr = positions_grad.data_ptr<double>();
+
+        for (size_t block_i=0; block_i<n_blocks; block_i++) {
+            auto gradient = descriptor->block_by_id(block_i)->gradient("positions");
+
+            auto samples = gradient->samples();
+            auto samples_values = samples->values();
+            auto samples_values_ptr = samples_values.data_ptr<int32_t>();
+            always_assert(samples->names().size() == 3);
+            always_assert(samples->names()[0] == "sample");
+            always_assert(samples->names()[1] == "structure");
+            always_assert(samples->names()[2] == "atom");
+
+            // This is dX/ dr_i, which we computed in the forward pass
+            auto forward_values = gradient->values();
+            always_assert(forward_values.is_contiguous() && forward_values.is_cpu());
+            auto forward_grad_ptr = forward_values.data_ptr<double>();
+
+            // This is dA/ dX, which torch computed in the beginning of the
+            // backward pass
+            auto grad_output = grad_outputs[block_i];
+            const auto& grad_output_sizes = grad_output.sizes();
+            always_assert(grad_output.is_contiguous() && grad_output.is_cpu());
+            auto grad_values_ptr = grad_output.data_ptr<double>();
+
+            // total size of component + property dimension
+            int64_t dot_dimensions = 1;
+            for (int i=1; i<grad_output_sizes.size(); i++) {
+                dot_dimensions *= grad_output_sizes[i];
+            }
+
+            // We want to compute dA/dr_i using the data above to finish the
+            // backward pass
+            for (int64_t grad_sample_i=0; grad_sample_i<samples->count(); grad_sample_i++) {
+                auto sample_i = samples_values_ptr[grad_sample_i * 3 + 0];
+                auto structure_i = samples_values_ptr[grad_sample_i * 3 + 1];
+                auto atom_i = samples_values_ptr[grad_sample_i* 3 + 2];
+
+                auto global_atom_i = structures_start[structure_i] + atom_i;
+
+                for (int64_t direction=0; direction<3; direction++) {
+                    auto dA_dr = 0.0;
+                    for (int64_t i=0; i<dot_dimensions; i++) {
+                        auto dX_dr = forward_grad_ptr[(grad_sample_i * 3 + direction) * dot_dimensions + i];
+                        auto dA_dX = grad_values_ptr[sample_i * dot_dimensions + i];
+                        dA_dr += dX_dr * dA_dX;
+                    }
+                    positions_grad_ptr[global_atom_i * 3 + direction] += dA_dr;
+                }
+            }
+        }
+    }
+
+    /* gradient w.r.t. cell */
+    auto cell_grad = torch::Tensor();
+    if (all_cells.requires_grad()) {
+        cell_grad = torch::zeros_like(all_cells);
+        always_assert(cell_grad.is_contiguous() && cell_grad.is_cpu());
+        auto cell_grad_ptr = cell_grad.data_ptr<double>();
+
+        // find the index of the "structure" dimension in the samples
+        auto sample_names = descriptor->sample_names();
+        auto structure_dimension_it = std::find(
+            std::begin(sample_names),
+            std::end(sample_names),
+            "structure"
+        );
+        if (structure_dimension_it == std::end(sample_names)) {
+            C10_THROW_ERROR(ValueError,
+                "could not find 'structure' in the samples, this calculator is missing it"
+            );
+        }
+        int64_t structure_dimension = std::distance(std::begin(sample_names), structure_dimension_it);
+
+        for (size_t block_i=0; block_i<n_blocks; block_i++) {
+            auto block = descriptor->block_by_id(block_i);
+            auto gradient = block->gradient("cell");
+
+            auto structures = block->samples()->values().index({torch::indexing::Slice(), structure_dimension});
+
+            auto samples = gradient->samples();
+            auto samples_values = samples->values();
+            auto samples_values_ptr = samples_values.data_ptr<int32_t>();
+            always_assert(samples->names().size() == 1);
+            always_assert(samples->names()[0] == "sample");
+
+            // This is dX/ dH, which we computed in the forward pass
+            auto forward_values = gradient->values();
+            always_assert(forward_values.is_contiguous() && forward_values.is_cpu());
+            auto forward_grad_ptr = forward_values.data_ptr<double>();
+
+            // This is dA/ dX, which torch computed in the beginning of the
+            // backward pass
+            auto grad_output = grad_outputs[block_i];
+            const auto& grad_output_sizes = grad_output.sizes();
+            always_assert(grad_output.is_contiguous() && grad_output.is_cpu());
+            auto grad_values_ptr = grad_output.data_ptr<double>();
+
+            // total size of component + property dimension
+            int64_t dot_dimensions = 1;
+            for (int i=1; i<grad_output_sizes.size(); i++) {
+                dot_dimensions *= grad_output_sizes[i];
+            }
+
+            // We want to compute dA/dH using the data above to finish the
+            // backward pass
+            for (int64_t grad_sample_i=0; grad_sample_i<samples->count(); grad_sample_i++) {
+                auto sample_i = samples_values_ptr[grad_sample_i];
+                // we get the structure from the samples of the values
+                auto structure_i = structures[sample_i].item<int32_t>();
+
+                for (int64_t direction_1=0; direction_1<3; direction_1++) {
+                    for (int64_t direction_2=0; direction_2<3; direction_2++) {
+                        auto dA_dH = 0.0;
+                        for (int64_t i=0; i<dot_dimensions; i++) {
+                            auto id = (grad_sample_i * 3 + direction_2) * 3 + direction_1;
+                            auto dX_dH = forward_grad_ptr[id * dot_dimensions + i];
+                            auto dA_dX = grad_values_ptr[sample_i * dot_dimensions + i];
+                            dA_dH += dX_dH * dA_dX;
+                        }
+                        cell_grad_ptr[(structure_i * 3 + direction_1) * 3 + direction_2] += dA_dH;
+                    }
+                }
+            }
+        }
+    }
+
+    return {
+        positions_grad,
+        cell_grad,
+        torch::Tensor(),
+        torch::Tensor(),
+        torch::Tensor(),
+    };
+}
--- rascaline-torch/src/calculator.cpp
+++ rascaline-torch/src/calculator.cpp
@@ -1,1 +1,102 @@
 #include "rascaline/torch/calculator.hpp"
+#include "rascaline/torch/autograd.hpp"
+
+using namespace rascaline_torch;
+
+// move a block created by rascaline to torch
+static equistore::TensorBlock block_to_torch(
+    std::shared_ptr<equistore::TensorMap> tensor,
+    equistore::TensorBlock block
+) {
+    auto values = block.values();
+    auto sizes = std::vector<int64_t>();
+    for (auto size: values.shape()) {
+        sizes.push_back(static_cast<int64_t>(size));
+    }
+
+    auto torch_values = torch::from_blob(
+        values.data(),
+        std::move(sizes),
+        [tensor](void*){
+            // this function holds a copy of `tensor`, which will make sure that
+            // (a) the TensorMap is kept alive for as long as the values
+            // returned by `torch::from_blob` is; and (b) the TensorMap will be
+            // freed once all the torch::Tensor created with `torch::from_blob`
+            // are freed as well
+            auto _ = std::move(tensor);
+        },
+        torch::TensorOptions().dtype(torch::kF64).device(torch::kCPU)
+    );
+
+    auto new_block = equistore::TensorBlock(
+        std::unique_ptr<equistore::DataArrayBase>(new equistore_torch::TorchDataArray(std::move(torch_values))),
+        block.samples(),
+        block.components(),
+        block.properties()
+    );
+
+    for (auto parameter: block.gradients_list()) {
+        auto gradient = block_to_torch(tensor, block.gradient(parameter));
+        new_block.add_gradient(std::move(parameter), std::move(gradient));
+    }
+
+    return new_block;
+}
+
+
+equistore_torch::TorchTensorMap CalculatorHolder::compute_impl(
+    std::vector<TorchSystem>& systems,
+    rascaline::CalculationOptions options
+) {
+    // convert the systems
+    auto base_systems = std::vector<rascal_system_t>();
+    base_systems.reserve(systems.size());
+    for (auto& system: systems) {
+        base_systems.push_back(system->as_rascal_system_t());
+    }
+
+    // run the calculations
+    auto descriptor = std::make_shared<equistore::TensorMap>(calculator_.compute(base_systems, options));
+
+    // move all data to torch
+    auto blocks = std::vector<equistore::TensorBlock>();
+    for (size_t block_i=0; block_i<descriptor->keys().count(); block_i++) {
+        blocks.push_back(block_to_torch(descriptor, descriptor->block_by_id(block_i)));
+    }
+
+    return torch::make_intrusive<equistore_torch::TensorMapHolder>(
+        equistore::TensorMap(descriptor->keys(), std::move(blocks))
+    );
+}
+
+
+equistore_torch::TorchTensorMap CalculatorHolder::compute(
+    std::vector<TorchSystem> systems
+) {
+    auto all_positions_vec = std::vector<torch::Tensor>();
+    all_positions_vec.reserve(systems.size());
+
+    auto all_cells_vec = std::vector<torch::Tensor>();
+    all_cells_vec.reserve(systems.size());
+
+    for (const auto& system: systems) {
+        all_positions_vec.push_back(system->get_positions());
+        all_cells_vec.push_back(system->get_cell());
+    }
+
+    auto all_positions = torch::vstack(all_positions_vec);
+    auto all_cells = torch::vstack(all_cells_vec);
+
+    auto descriptor = equistore_torch::TorchTensorMap();
+
+    // see `RascalineAutograd::forward` for an explanation of what's happening
+    auto _ = RascalineAutograd::apply(
+        all_positions,
+        all_cells,
+        *this,
+        systems,
+        &descriptor
+    );
+
+    return descriptor;
+}
