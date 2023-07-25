ef4c3af79190095f0f2544b08926adbc470bdcbc
Guillaume Fraux <guillaume.fraux@epfl.ch>
2023-07-25 13:35:54 +0200

    Expose key selections to the C++ interface

--- rascaline-c-api/tests/cxx/calculator.cpp
+++ rascaline-c-api/tests/cxx/calculator.cpp
@@ -416,4 +416,52 @@ TEST_CASE("Compute descriptor") {
             {2, 3, 1}
         ));
     }
+
+    SECTION("Partial compute -- key selection") {
+        // check key selection: we add a non-existing key (12) and remove an
+        // existing one (1) from the default set of keys. We also put the keys
+        // in a different order than what would be the default (6, 12).
+
+        auto options = rascaline::CalculationOptions();
+        options.selected_keys = equistore::Labels(
+            {"species_center"},
+            {{12}, {6}}
+        );
+        auto descriptor = calculator.compute(systems, options);
+
+        CHECK(descriptor.keys() == equistore::Labels(
+            {"species_center"},
+            {{12}, {6}}
+        ));
+
+        // empty block
+        auto block = descriptor.block_by_id(0);
+        CHECK(block.samples() == equistore::Labels(
+            {"structure", "center"},
+            {}
+        ));
+        CHECK(block.properties() == equistore::Labels(
+            {"index_delta", "x_y_z"},
+            {{1, 0}, {0, 1}}
+        ));
+        CHECK(block.values() == equistore::NDArray<double>(
+            std::vector<double>{},
+            {0, 2}
+        ));
+
+        // C block
+        block = descriptor.block_by_id(1);
+        CHECK(block.samples() == equistore::Labels(
+            {"structure", "center"},
+            {{0, 0}}
+        ));
+        CHECK(block.properties() == equistore::Labels(
+            {"index_delta", "x_y_z"},
+            {{1, 0}, {0, 1}}
+        ));
+        CHECK(block.values() == equistore::NDArray<double>(
+            {4.0, 3.0},
+            {1, 2}
+        ));
+    }
 }
