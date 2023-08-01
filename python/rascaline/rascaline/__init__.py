62d40815f52a010452d4679517abd143c57a4366
Philip Loche <ploche@physik.fu-berlin.de>
2023-08-01 14:32:59 +0200

    Add flake8-sphinx-links to the linter

--- python/rascaline/rascaline/__init__.py
+++ python/rascaline/rascaline/__init__.py
@@ -1,20 +1,21 @@
 from . import utils  # noqa
 from .calculator_base import CalculatorBase  # noqa
+
+# don't forget to also update `rascaline/torch/__init__.py` and
+# `rascaline/torch/calculators.py` when modifying this file
+from .calculators import (
+    AtomicComposition,
+    LodeSphericalExpansion,
+    NeighborList,
+    SoapPowerSpectrum,
+    SoapRadialSpectrum,
+    SortedDistances,
+    SphericalExpansion,
+    SphericalExpansionByPair,
+)
 from .log import set_logging_callback  # noqa
 from .profiling import Profiler  # noqa
 from .splines import generate_splines  # noqa
 from .status import RascalError  # noqa
 from .systems import IntoSystem, SystemBase  # noqa
 from .version import __version__  # noqa
-
-
-# don't forget to also update `rascaline/torch/__init__.py` and
-# `rascaline/torch/calculators.py` when modifying this file
-from .calculators import AtomicComposition  # noqa  isort: skip
-from .calculators import SortedDistances  # noqa  isort: skip
-from .calculators import NeighborList  # noqa  isort: skip
-from .calculators import LodeSphericalExpansion  # noqa isort: skip
-from .calculators import SphericalExpansion  # noqa  isort: skip
-from .calculators import SphericalExpansionByPair  # noqa  isort: skip
-from .calculators import SoapRadialSpectrum  # noqa  isort: skip
-from .calculators import SoapPowerSpectrum  # noqa  isort: skip
