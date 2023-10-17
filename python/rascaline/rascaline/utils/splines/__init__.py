581d0ca4dece424b4594c8c3cac40a7381e13ae5
Philip Loche <ploche@physik.fu-berlin.de>
2023-10-17 10:54:09 +0200

    Added real and Fourier space spliner base classes (#235)
    Co-authored-by: Guillaume Fraux <guillaume.fraux@epfl.ch>

---
+++ python/rascaline/rascaline/utils/splines/__init__.py
@@ -0,0 +1,18 @@
+from .atomic_density import (  # noqa
+    AtomicDensityBase,
+    DeltaDensity,
+    GaussianDensity,
+    LodeDensity,
+)
+from .radial_basis import (  # noqa
+    GtoBasis,
+    MonomialBasis,
+    RadialBasisBase,
+    SphericalBesselBasis,
+)
+from .splines import (  # noqa
+    LodeSpliner,
+    RadialIntegralFromFunction,
+    RadialIntegralSplinerBase,
+    SoapSpliner,
+)
