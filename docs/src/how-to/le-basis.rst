5478a5eca45962d6c06a875468059a3e55968948
frostedoyster <bigi.f@libero.it>
2023-11-04 20:19:56 +0100

    Draft

---
+++ docs/src/how-to/le-basis.rst
@@ -0,0 +1,34 @@
+Laplacian eigenstate basis
+==========================
+
+This examples shows how to calculate a spherical expansion using a Laplacian eigenstate basis.
+
+.. tabs::
+
+    .. group-tab:: Python
+
+        .. container:: sphx-glr-footer sphx-glr-footer-example
+
+            .. container:: sphx-glr-download sphx-glr-download-python
+
+                :download:`Download Python source code for this example: tabulated.py <../examples/le-basis.py>`
+
+            .. container:: sphx-glr-download sphx-glr-download-jupyter
+
+                :download:`Download Jupyter notebook for this example: tabulated.ipynb <../examples/le-basis.ipynb>`
+
+        .. include:: ../examples/le-basis.rst
+            :start-after: start-body
+            :end-before: end-body
+
+    .. group-tab:: Rust
+
+        To be done
+
+    .. group-tab:: C++
+
+        To be done
+
+    .. group-tab:: C
+
+        To be done
---
+++ python/rascaline/examples/le-basis.py
@@ -0,0 +1,203 @@
+"""
+.. _example-le-basis:
+
+Laplacian eigenstate basis
+==========================
+
+.. start-body
+"""
+
+# %%
+#
+# This script illustrates how to generate a spherical expansion using
+# the Laplacian eigenstate basis (https://doi.org/10.1063/5.0124363),
+# both using truncation with l_max, n_max hyper-parameters and with an
+# eigenvalue threshold.
+
+import ase.io
+import numpy as np
+import scipy.optimize
+from scipy.special import jv
+
+from metatensor import Labels, TensorBlock, TensorMap
+import rascaline
+
+try:
+    import torch
+    torch.set_default_dtype(torch.float64)
+    import torch_spex
+    HAS_TORCH_SPEX = True
+except ImportError:
+    HAS_TORCH_SPEX = False
+
+# %%
+#
+# First using a truncation with l_max, n_max hyper-parameters (easy):
+cutoff = 4.4
+l_max = 4
+n_max = 6
+
+structures = ase.io.read("dataset.xyz", ":10")
+
+spliner = rascaline.utils.SoapSpliner(
+    cutoff=cutoff,
+    max_radial=n_max,
+    max_angular=l_max,
+    basis=rascaline.utils.SphericalBesselBasis(cutoff=cutoff, max_radial=n_max, max_angular=l_max),
+    density=rascaline.utils.DeltaDensity(),
+    accuracy=1e-8,
+)
+
+calculator = rascaline.SphericalExpansion(
+    cutoff=cutoff,
+    max_radial=n_max,
+    max_angular=l_max,
+    center_atom_weight=1.0,
+    radial_basis=spliner.compute(),
+    atomic_gaussian_width=-1.0,  # will not be used due to the delta density above
+    cutoff_function={"ShiftedCosine": {"width": 0.5}},
+)
+
+spherical_expansion = calculator.compute(structures)
+
+# %%
+#
+# Now we will calculate the same basis with an eigenvalue threshold
+# (more involved), which affords a better accuracy/cost ratio, using
+# property selection.
+
+E_max = 60  # eigenvalue threshold
+
+# %%
+#
+# Spherical Bessel zeros (from the scipy cookbook) and
+# Laplacian eigenvalues
+
+def Jn(r, n):
+    return np.sqrt(np.pi / (2 * r)) * jv(n + 0.5, r)
+
+def Jn_zeros(n, nt):
+    zeros_j = np.zeros((n + 1, nt), dtype=np.float64)
+    zeros_j[0] = np.arange(1, nt + 1) * np.pi
+    points = np.arange(1, nt + n + 1) * np.pi
+    roots = np.zeros(nt + n, dtype=np.float64)
+    for i in range(1, n + 1):
+        for j in range(nt + n - i):
+            roots[j] = scipy.optimize.brentq(Jn, points[j], points[j + 1], (i,))
+        points = roots
+        zeros_j[i][:nt] = roots[:nt]
+    return zeros_j
+
+l_max_large = 50  # just used to get the eigenvalues
+n_max_large = 50  # just used to get the eigenvalues
+z_ln = Jn_zeros(l_max_large, n_max_large)
+
+E_ln = z_ln**2  # proportional to the Laplacian eigenvalues, which would be z_ln**2 / cutoff**2
+
+# %%
+#
+# Determine the l_max, n_max parameters that will certainly
+# contain all the desired terms
+
+n_max_l = []
+for l in range(l_max_large+1):
+    # for each l, calculate how many basis functions
+    n_max_l.append(
+        len(np.where(E_ln[l]<E_max)[0])
+    )
+    if n_max_l[-1] == 0:
+        # all eigenvalues for this l are over the threshold
+        n_max_l.pop()
+        l_max = l-1
+        break
+
+n_max = n_max_l[0]
+
+# %%
+#
+# Set up a TensorMap for property selection
+
+all_species = list(np.unique(np.concatenate([structure.numbers for structure in structures])))  # extract all the species from the small dataset
+
+keys = []
+blocks = []
+for center_species in all_species:
+    for neighbor_species in all_species:
+        for l in range(l_max+1):
+            keys.append([l, center_species, neighbor_species])
+            blocks.append(
+                TensorBlock(
+                    values=np.empty((1, n_max_l[l])),
+                    samples=Labels.single(),
+                    components=[],
+                    properties=Labels(
+                        names=["n"],
+                        values=np.arange(n_max_l[l]).reshape(n_max_l[l], 1)
+                    )
+                )
+            )
+
+selected_properties = TensorMap(
+    keys=Labels(
+        names=["spherical_harmonics_l", "species_center", "species_neighbor"],
+        values=np.array(keys)
+    ),
+    blocks=blocks
+)
+
+# %%
+#
+# Build a calculator and calculate the spherical expansion
+
+spliner = rascaline.utils.SoapSpliner(
+    cutoff=cutoff,
+    max_radial=n_max,
+    max_angular=l_max,
+    basis=rascaline.utils.SphericalBesselBasis(cutoff=cutoff, max_radial=n_max, max_angular=l_max),
+    density=rascaline.utils.DeltaDensity(),
+    accuracy=1e-8,
+)
+
+calculator = rascaline.SphericalExpansion(
+    cutoff=cutoff,
+    max_radial=n_max,
+    max_angular=l_max,
+    center_atom_weight=1.0,
+    radial_basis=spliner.compute(),
+    atomic_gaussian_width=-1.0,  # will not be used due to the delta density above
+    cutoff_function={"Step": {}},
+)
+
+spherical_expansion = calculator.compute(
+    structures,
+    selected_properties=selected_properties  # we tell the calculator to only compute the selected properties
+)
+
+if HAS_TORCH_SPEX:
+    # check that the results are the same as those from torch_spex
+    from torch_spex.spherical_expansions import SphericalExpansion
+    from torch_spex.structures import InMemoryDataset, TransformerNeighborList, collate_nl
+    hypers = {
+        "cutoff radius": cutoff,
+        "radial basis": {
+            "type": "le",
+            "mlp": False,
+            "r_cut": cutoff,
+            "E_max": E_max,
+            "normalize": False
+        }
+    }
+
+    transformers = [TransformerNeighborList(cutoff=cutoff, device="cpu")]
+    dataset = InMemoryDataset(structures, transformers)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=10, shuffle=False, collate_fn=collate_nl)
+    structures = next(iter(dataloader))
+
+    torch_spex_calculator = SphericalExpansion(hypers, all_species, device="cpu")
+    torch_spex_spherical_expansion = torch_spex_calculator(**structures)
+    spherical_expansion = spherical_expansion.keys_to_properties(["species_neighbor"])
+
+    block = spherical_expansion.block(spherical_harmonics_l=1, species_center=1)
+    torch_spex_block = torch_spex_spherical_expansion.block({"lam":1, "a_i":1, "sigma":1})
+
+    assert np.allclose(block.values[:, :, 1], torch_spex_block.values.numpy()[:, :, 1])
