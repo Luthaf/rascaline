88a1c2ca0d8c6acdaff6124d82f5116130fbece7
Filippo Bigi <98903385+frostedoyster@users.noreply.github.com>
2024-09-05 11:26:21 +0200

    Update intersphinx for metatensor

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -141,7 +141,7 @@ breathe_domain_by_extension = {
 intersphinx_mapping = {
     "ase": ("https://wiki.fysik.dtu.dk/ase/", None),
     "chemfiles": ("https://chemfiles.org/chemfiles.py/latest/", None),
-    "metatensor": ("https://lab-cosmo.github.io/metatensor/latest/", None),
+    "metatensor": ("https://metatensor.github.io/metatensor/latest/", None),
     "matplotlib": ("https://matplotlib.org/stable/", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
     "scipy": ("https://docs.scipy.org/doc/scipy/", None),
