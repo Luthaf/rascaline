06ec4ff629be42568cb8e530f10fa39c1f048155
Guillaume Fraux <guillaume.fraux@epfl.ch>
2024-11-27 14:24:02 +0100

    Do not check patch version of torch when loading featomic_torch from cmake

--- featomic-torch/cmake/featomic_torch-config.in.cmake
+++ featomic-torch/cmake/featomic_torch-config.in.cmake
@@ -8,11 +8,21 @@ find_package(featomic ${REQUIRED_FEATOMIC_VERSION} CONFIG REQUIRED)
 set(REQUIRED_METATENSOR_TORCH_VERSION @REQUIRED_METATENSOR_TORCH_VERSION@)
 find_package(metatensor_torch ${REQUIRED_METATENSOR_TORCH_VERSION} CONFIG REQUIRED)
 
-# We can only load featomic_torch with the exact same version of Torch that
-# was used to compile it (and is stored in BUILD_TORCH_VERSION)
+# We can only load metatensorfeatomic_torch with the same minor version of Torch
+# that was used to compile it (and is stored in BUILD_TORCH_VERSION)
 set(BUILD_TORCH_VERSION @Torch_VERSION@)
+set(BUILD_TORCH_MAJOR @Torch_VERSION_MAJOR@)
+set(BUILD_TORCH_MINOR @Torch_VERSION_MINOR@)
 
-find_package(Torch ${BUILD_TORCH_VERSION} REQUIRED EXACT)
+find_package(Torch ${BUILD_TORCH_VERSION} REQUIRED)
+
+if (NOT "${BUILD_TORCH_MAJOR}" STREQUAL "${Torch_VERSION_MAJOR}")
+    message(FATAL_ERROR "found incompatible torch version: featomic-torch was built against v${BUILD_TORCH_VERSION} but we found v${Torch_VERSION}")
+endif()
+
+if (NOT "${BUILD_TORCH_MINOR}" STREQUAL "${Torch_VERSION_MINOR}")
+    message(FATAL_ERROR "found incompatible torch version: featomic-torch was built against v${BUILD_TORCH_VERSION} but we found v${Torch_VERSION}")
+endif()
 
 
 include(${CMAKE_CURRENT_LIST_DIR}/featomic_torch-targets.cmake)
