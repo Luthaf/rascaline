31e828108363c1c2f3cebfffdf05e40ca4e40cba
Luthaf <Luthaf@users.noreply.github.com>
2024-12-10 13:00:30 +0000

    deploy: 368032010f06dec46a59f3fd7fb3720c07f53c69

---
+++ featomic-v0.6.0-rc1/references/api/rust/trait.impl/rayon/iter/trait.ParallelIterator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["metatensor",[["impl&lt;'a&gt; ParallelIterator for <a class=\"struct\" href=\"metatensor/struct.LabelsParIter.html\" title=\"struct metatensor::LabelsParIter\">LabelsParIter</a>&lt;'a&gt;"],["impl&lt;'a&gt; ParallelIterator for <a class=\"struct\" href=\"metatensor/struct.TensorMapParIter.html\" title=\"struct metatensor::TensorMapParIter\">TensorMapParIter</a>&lt;'a&gt;"],["impl&lt;'a&gt; ParallelIterator for <a class=\"struct\" href=\"metatensor/struct.TensorMapParIterMut.html\" title=\"struct metatensor::TensorMapParIterMut\">TensorMapParIterMut</a>&lt;'a&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[574]}
